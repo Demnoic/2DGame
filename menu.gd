7e6147aa68d40c3b722a4e01e9c5befec8932c47
Max Kurth <30091369@student.toiohomai.ac.nz>
2024-05-17 10:32:43 +1200

    first commit

---
+++ menu.gd
@@ -0,0 +1,15 @@
+extends Control
+
+func _process(delta):
+	pass
+
+func _on_hostbutton_pressed():
+	pass # Replace with function body.
+
+
+func _on_join_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_start_button_pressed():
+	pass # Replace with function body.
